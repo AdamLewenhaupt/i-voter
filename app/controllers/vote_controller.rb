3cfde094c194c1276fe4bc99e27a7b8160255a53
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2015-03-31 13:30:47 +0200

    try2

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -2,30 +2,30 @@ class VoteController < FayeRails::Controller
 
 	voters = 0
 
-	# channel '/vote' do
-	# 	monitor :subscribe do
-	# 		voters += 1
-	# 		VoteController.publish('/vote', "count:#{voters}")
-	# 	end
+	channel '/vote' do
+		monitor :subscribe do
+			voters += 1
+			VoteController.publish('/vote', "count:#{voters}")
+		end
 
-	# 	monitor :unsubscribe do
-	# 		voters -= 1
-	# 		VoteController.publish('/vote', "count:#{voters}")
-	# 	end
+		monitor :unsubscribe do
+			voters -= 1
+			VoteController.publish('/vote', "count:#{voters}")
+		end
 
-	# 	monitor :publish do 
-	# 		msg = data.inspect
-	# 		if msg != nil
+		monitor :publish do 
+			msg = data.inspect
+			if msg != nil
 
-	# 			parts = msg.delete('"').split ":"
-	# 			if parts[0] == "vote"
-	# 				id = parts[1].to_i
-	# 				$voteCounter[$voteOptions[id]] += 1
-	# 				VoteController.publish '/vote', "admin:#{id}"
-	# 			end
-	# 		end
-	# 	end
-	# end
+				parts = msg.delete('"').split ":"
+				if parts[0] == "vote"
+					id = parts[1].to_i
+					$voteCounter[$voteOptions[id]] += 1
+					VoteController.publish '/vote', "admin:#{id}"
+				end
+			end
+		end
+	end
 
 	$setVote = lambda do |options|
 		opts = options.join '|'
