9b4b593dc57d9f6720aac511bbec7f15c5501c1f
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2015-03-31 13:37:21 +0200

    try5

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -3,28 +3,28 @@ class VoteController < FayeRails::Controller
 	voters = 0
 
 	channel '/vote' do
-		monitor :subscribe do
-			voters += 1
-			VoteController.publish('/vote', "count:#{voters}")
-		end
-
-		monitor :unsubscribe do
-			voters -= 1
-			VoteController.publish('/vote', "count:#{voters}")
-		end
-
-		# monitor :publish do 
-		# 	msg = data.inspect
-		# 	if msg != nil
-
-		# 		parts = msg.delete('"').split ":"
-		# 		if parts[0] == "vote"
-		# 			id = parts[1].to_i
-		# 			$voteCounter[$voteOptions[id]] += 1
-		# 			VoteController.publish '/vote', "admin:#{id}"
-		# 		end
-		# 	end
+		# monitor :subscribe do
+		# 	voters += 1
+		# 	VoteController.publish('/vote', "count:#{voters}")
 		# end
+
+		# monitor :unsubscribe do
+		# 	voters -= 1
+		# 	VoteController.publish('/vote', "count:#{voters}")
+		# end
+
+		monitor :publish do 
+			msg = data.inspect
+			if msg != nil
+
+				parts = msg.delete('"').split ":"
+				if parts[0] == "vote"
+					id = parts[1].to_i
+					$voteCounter[$voteOptions[id]] += 1
+					VoteController.publish '/vote', "admin:#{id}"
+				end
+			end
+		end
 	end
 
 	$setVote = lambda do |options|
